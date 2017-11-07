42b67677b388f54fe3bfeee4af0124ac215a7d41
acwhi <ac,white01@gmail.com>
2017-11-07 12:03:27 -0500

    Added function to go past the month into another month. throwing a Number Format error at the moment when the day goes past the month.

--- TakeHomeTest3/src/Date.java
+++ TakeHomeTest3/src/Date.java
@@ -3,6 +3,7 @@ public class Date {
     private String month;
     private int day;
     private String year;
+    private int daysOfTheMonth;
 
     public Date() {
        month = "";
@@ -17,7 +18,12 @@ public class Date {
     }
 
     public void addDays(int addedDays) {
-        day = addedDays + day;
+        day  = day + addedDays;
+        if(day > daysOfTheMonth) {
+            day = daysOfTheMonth - day;
+            month = "" + (Integer.parseInt(month) + 1);
+        }
+
     }
 
     public int subtractDays(int subDays) {
@@ -31,15 +37,51 @@ public class Date {
     public void months(String month) {
         switch(month) {
             case "January":
-                month = "1";
+                this.month = "1";
+                daysOfTheMonth = 31;
                 break;
             case "Febuary":
-                month = "2";
+                this.month = "2";
+                daysOfTheMonth = 28;
                 break;
             case "March":
-                month = "3";
+                this.month = "3";
+                daysOfTheMonth = 31;
             case "April":
-                month = "4";
+                this.month = "4";
+                daysOfTheMonth = 30;
+                break;
+            case "May":
+                this.month = "5";
+                daysOfTheMonth = 31;
+                break;
+            case "June":
+                this.month = "6";
+                daysOfTheMonth = 30;
+                break;
+            case "July":
+                this.month = "7";
+                daysOfTheMonth = 31;
+                break;
+            case "August":
+                this.month = "8";
+                daysOfTheMonth = 31;
+                break;
+            case "September":
+                this.month = "9";
+                daysOfTheMonth = 30;
+                break;
+            case "October":
+                this.month = "10";
+                daysOfTheMonth = 31;
+                break;
+            case "November":
+                this.month = "11";
+                daysOfTheMonth = 30;
+                break;
+            case "December":
+                this.month = "12";
+                daysOfTheMonth = 30;
                 break;
         }
     }

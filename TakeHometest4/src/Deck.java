cd38c2f6ffbff951a96729adc1d17304c3767724
Austin <ac.white01@gmail.com>
2017-11-29 08:30:37 -0500

    finished the card creation. Started working on the shuffle method.

--- TakeHometest4/src/Deck.java
+++ TakeHometest4/src/Deck.java
@@ -1,5 +1,9 @@
+import java.util.Random;
+
 public class Deck {
-    Card[] card = new Card[52];
+    private Random rand = new Random();
+    private Card[] card = new Card[52];
+    private int referenceOfCard;
 
 
 
@@ -12,14 +16,27 @@ public class Deck {
         for(int i = 0; i < suit.length; i++) {
             for(int j = 0; j < face.length; j++) {
 
-                card[count] =  new Card(face[j], suit[i], j);
 
-
-
+                if(face[j].equals("J") || face[j].equals("Q") || face[j].equals("K")) {
+                    card[count] =  new Card(face[j], suit[i], 10);
+                } else if(face[j].equals("A")) {
+                    card[count] =  new Card(face[j], suit[i], 11);
+                } else {
+                    card[count] =  new Card(face[j], suit[i], j + 2);
+                }
                 count++;
             }
         }
 
+    }
+
+    public void shuffle() {
+        for(int i = 0; i < 1000; i++) {
+            int place1 = rand.nextInt(52);
+            int place2 = rand.nextInt(52);
+
+
+        }
     }
 
 
--- TakeHometest4/src/GameLogic.java
+++ TakeHometest4/src/GameLogic.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class GameLogic {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Deck deck = new Deck();
 
-        for(int i = 0; i < deck.card.length; i++) {
-            System.out.println(deck.card[i]);
-            System.out.println();
-        }
+        String user;
+
+        do {
+
+
+            user = input.nextLine();
+        }while(user.toLowerCase().equals("q"));
+
+
     }
 }
